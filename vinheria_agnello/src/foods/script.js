8f03453c4842c012bda9b86a8fb5fe2155efdbd2
Arthur Pagani <112992377+acpagani@users.noreply.github.com>
2024-04-30 19:06:40 -0300

    finalização js-vinheria

--- vinheria_agnello/src/foods/script.js
+++ vinheria_agnello/src/foods/script.js
@@ -1,13 +1,12 @@
 function showFoods() {
     let harmo = document.querySelector("#harmo")
     let img = document.querySelector("#imagem-vinho")
-    let src_img;
     let img_description = document.querySelector(".harmo__img")
     let harmo_text;
     let wine_id = document.cookie.split('=')[1]
 
     if (wine_id == 1) {
-        src_img = "/vinheria_agnello/assets/img/first-wine.png"
+        img.src = "/vinheria_agnello/assets/img/first-wine.png"
         harmo_text = `<ul>
         <li>Carnes Vermelhas grelhadas e assadas</li>
         <li>Queijos Fortes</li>
@@ -15,7 +14,7 @@ function showFoods() {
         img_description.innerHTML += "<h2>Aliara Odfjell</h2>"
     }
     else if (wine_id == 2) {
-        src_img = "/vinheria_agnello/assets/img/second-wine.png"
+        img.src = "/vinheria_agnello/assets/img/second-wine.png"
         harmo_text = `<ul>
         <li>Carnes Vermelhas</li>
         <li>Gastronomia Mediterrânea</li>
@@ -23,7 +22,6 @@ function showFoods() {
         img_description.innerHTML += "<h2>Château Hyot Prestige</h2>"
     }
 
-    img.src = src_img
     harmo.innerHTML += harmo_text
 }
 
